b5512d7594b0be3d871c96bafe40222d4c1d2f1f
Sushishu <90468138+lucky-sushishu@users.noreply.github.com>
2024-04-11 22:56:52 +0800

    引入反正切函数把加速度转换为俯仰角和姿态角

--- src/application/module/imu/imu.c
+++ src/application/module/imu/imu.c
@@ -1,5 +1,6 @@
 #include "imu.h"
 #include "mpu9250.h"
+#include "math.h"
 
 TX_THREAD imu_mag_tcb;
 UCHAR imu_mag_stack[IMU_MAG_STACKSIZE];
@@ -27,11 +28,21 @@ void imu_mag_entry(ULONG thread_input)
     mpu9250_get_acce((mpu9250_data_t *)&sensor_imu.acce);
     mpu9250_get_mag((mpu9250_data_t *)&sensor_mag.data);
 
-    // if(count++ % 500 == 0)
-    // {
-    //   printf("acce: %.2f %.2f %.2f, gyro: %.4f %.4f %.4f\n", sensor_imu.acce[0], sensor_imu.acce[1], sensor_imu.acce[2],
-    //        sensor_imu.gyro[0], sensor_imu.gyro[1], sensor_imu.gyro[2]);
-    // }
+    /* 坐标系转换为前右下 */
+    double ax, ay, az;
+    ax = sensor_imu.acce[1];
+    ay = sensor_imu.acce[0];
+    az = sensor_imu.acce[2];
+    double pitch, roll;
+    pitch = atan2(ax, sqrt(ay*ay + az*az)) * 57.3f;
+    roll = atan2(ay, az) * 57.3f;
+
+    if(count++ % 250 == 0)
+    {
+      printf("pitch:%f, roll:%f", pitch, roll);
+      // printf("acce: %.2f %.2f %.2f, gyro: %.4f %.4f %.4f\n", sensor_imu.acce[0], sensor_imu.acce[1], sensor_imu.acce[2],
+      //      sensor_imu.gyro[0], sensor_imu.gyro[1], sensor_imu.gyro[2]);
+    }
 
     /* 频率：250Hz */
     /* TODO: 似乎实际OS的tx_thread_sleep函数并不很精准，延时1000，实际在1000上下波动，但总体似乎是小于1000的，可能串口助手的机制不精准 */
