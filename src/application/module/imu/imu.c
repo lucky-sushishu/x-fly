0c90acd3c50b4f6f23ed496cb239d6788755f780
Sushishu <90468138+lucky-sushishu@users.noreply.github.com>
2024-12-17 23:19:21 +0800

    MahonAHRS: adjust MahonAHRS sampleFreq, adjust the right-hand coordinate system direction and compute euler by quaternions

--- src/application/module/imu/imu.c
+++ src/application/module/imu/imu.c
@@ -1,16 +1,64 @@
 #include "imu.h"
 #include "mpu9250.h"
 #include "math.h"
+#include "MahonyAHRS.h"
 
 TX_THREAD imu_mag_tcb;
 UCHAR imu_mag_stack[IMU_MAG_STACKSIZE];
 
 static sensor_imu_t sensor_imu;
 static float acce_bais[3], gyro_bais[3];
-static float acce_roll = 0, acce_pitch = 0, gyro_roll = 0, gyro_pitch = 0;
-static float roll_0 = 0, pitch_0 = 0;
 
-static void compute_imu_bais(void)
+static float roll = 0.0f, pitch = 0.0f, yaw = 0.0f;
+
+void compute_imu_bais(void);
+void q2euler(float q0, float q1, float q2, float q3, float *roll, float *pitch, float *yaw);
+
+void imu_mag_entry(ULONG thread_input)
+{
+  /* init */
+  float ax, ay, az, gx, gy, gz;
+
+  int count = 0;
+
+  if (mpu9250_init() != 0)
+  {
+    /* TODO : give led a message display imu init error */
+    printf("[sensor]: mpu9250 init error\n");
+    return;
+  }
+
+  compute_imu_bais();
+
+  while (1)
+  {
+    mpu9250_get_acce((mpu9250_data_t *)&sensor_imu.acce);
+    mpu9250_get_gyro((mpu9250_data_t *)&sensor_imu.gyro);
+
+    /* right-hand system */
+    ax = -(sensor_imu.acce[0] - acce_bais[0]) / 9.81f;
+    ay = -(sensor_imu.acce[1] - acce_bais[1]) / 9.81f;
+    az = (sensor_imu.acce[2] - acce_bais[2]) / 9.81f;
+    gx = -(sensor_imu.gyro[0] - gyro_bais[0]);
+    gy = -(sensor_imu.gyro[1] - gyro_bais[1]);
+    gz = (sensor_imu.gyro[2] - gyro_bais[2]);
+
+    MahonyAHRSupdateIMU(gx, gy, gz, ax, ay, az);
+
+    if(count++ % 250 == 0)
+    {
+      printf("ax:%f, ay:%f, az:%f.. gx:%f, gy:%f, gz:%f\r\n", ax, ay, az, gx, gy, gz);
+      q2euler(q0, q1, q2, q3, &roll, &pitch, &yaw);
+      printf("roll:%f, pitch:%f yaw:%f\r\n", roll * 57.3, pitch * 57.3, yaw * 57.3);
+    }
+
+    /* TODO : achieve more precise frequency */
+
+    tx_thread_sleep(2);
+  }
+}
+
+void compute_imu_bais(void)
 {
   int samples = 0;
   ULONG timestamp = tx_time_get();
@@ -41,72 +89,12 @@ static void compute_imu_bais(void)
   }
 }
 
-void imu_mag_entry(ULONG thread_input)
+/* Z-Y-X */
+void q2euler(float q0, float q1, float q2, float q3, float *roll, float *pitch, float *yaw)
 {
-  if (mpu9250_init() != 0)
-  {
-    /* TODO : give led a message display imu init error */
-    printf("[sensor]: mpu9250 init error\n");
-    return;
-  }
+  *roll = atan2(2.0f * (q0 * q1 + q2 * q3), 1.0f - 2.0f *(q1 * q1 + q2 * q2));
 
-  float ax, ay, az, gx, gy, gz;
+  *pitch = asin(2.0f * (q0 * q2 - q3 * q1));
 
-  int count = 0;
-  ULONG timestamp, sub_timestamp;
-  ULONG delay = 2;
-
-  compute_imu_bais();
-  printf("bais: acce: %f %f %f, gyro: %f %f %f\r\n", 
-          acce_bais[0], acce_bais[1], acce_bais[2], gyro_bais[0], gyro_bais[1], gyro_bais[2]);
-
-  /* 从加速度数据得到初始角度 */
-  mpu9250_get_acce((mpu9250_data_t *)&sensor_imu.acce);
-  ax = -(sensor_imu.acce[1] - acce_bais[1]);
-  ay = -(sensor_imu.acce[0] - acce_bais[0]);
-  az = -(sensor_imu.acce[2] - acce_bais[2]);
-  roll_0 = atan2(-ay, -az) * 57.3f;
-  pitch_0 = atan2(ax, sqrt(ay*ay + az*az)) * 57.3f;
-
-  gyro_roll += roll_0;
-  gyro_pitch += pitch_0;
-
-  while (1)
-  {
-    // timestamp = tx_time_get();
-
-    mpu9250_get_acce((mpu9250_data_t *)&sensor_imu.acce);
-    mpu9250_get_gyro((mpu9250_data_t *)&sensor_imu.gyro);
-
-    /* 坐标系转换为前右下 */
-    ax = -(sensor_imu.acce[1] - acce_bais[1]);
-    ay = -(sensor_imu.acce[0] - acce_bais[0]);
-    az = -(sensor_imu.acce[2] - acce_bais[2]);
-    gx = -(sensor_imu.gyro[1] - gyro_bais[1]);
-    gy = -(sensor_imu.gyro[0] - gyro_bais[0]);
-    gz = -(sensor_imu.gyro[2] - gyro_bais[2]);
-
-    acce_roll = atan2(-ay, -az) * 57.3f;
-    acce_pitch = atan2(ax, sqrt(ay*ay + az*az)) * 57.3f;
-
-    gyro_roll += gx * 57.3f * 0.002;
-    gyro_pitch += gy * 57.3f * 0.002;
-
-    if(count++ % 250 == 0)
-    {
-      printf("acce: roll:%f, pitch:%f, gyro: roll:%f, pitch:%f\r\n", acce_roll, acce_pitch, gyro_roll, gyro_pitch);
-      // printf("acce: %.2f %.2f %.2f, gyro: %.4f %.4f %.4f\n", sensor_imu.acce[0], sensor_imu.acce[1], sensor_imu.acce[2],
-      //      sensor_imu.gyro[0], sensor_imu.gyro[1], sensor_imu.gyro[2]);
-    }
-
-    /* 频率：250Hz */
-    /* TODO: 似乎实际OS的tx_thread_sleep函数并不很精准，延时1000，实际在1000上下波动，但总体似乎是小于1000的，可能串口助手的机制不精准 */
-    // sub_timestamp = tx_time_get() - timestamp;
-    // if(sub_timestamp < delay)
-    // {
-    //   tx_thread_sleep(delay - sub_timestamp);
-    // }
-
-    tx_thread_sleep(2);
-  }
+  *yaw = atan2(2.0f * (q0 * q3 + q1 * q2), 1.0f - 2.0f * (q2 * q2 + q3 * q3));
 }
