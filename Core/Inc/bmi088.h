fa844657ab99a29990a8537df5b59e0ef8784429
lucky-sushishu <sjcsushishu@outlook.com>
2026-06-19 21:40:46 +0800

    IMU：更新bmi088的驱动

--- Core/Inc/bmi088.h
+++ Core/Inc/bmi088.h
@@ -2,27 +2,63 @@
 #define BMI088_H
 
 #include <stdint.h>
+#include "imu.h"
 
+typedef struct bmi088_s bmi088_t;
 typedef enum bmi088_sensor_type_e bmi088_sensor_type_t;
+
+struct bmi088_s
+{
+	int16_t origin_accl_data[3];
+	int16_t origin_gyro_data[3];
+
+	imu_t   imu;
+};
 
 enum bmi088_sensor_type_e
 {
-	BMI088_ACCE,
+	BMI088_ACCL,
 	BMI088_GYRO,
 };
 
 /* bmi088 reg addr list */
-#define BMI088_ACCE_CHIP_ID		        0x00
-#define BMI088_ACCE_PWR_CONF		 	0x7C
-#define BMI088_ACCE_PWR_CTRL		  	0x7D
+#define BMI088_ACCL_CHIP_ID		        0x00
+#define BMI088_ACCL_DATA		  		0x12
+#define BMI088_ACCL_CHIP_ID_VALUE		0x1E
+#define BMI088_ACCL_RANGE				0x41
+#define BMI088_ACCL_RANGE_12G			0x02
+#define BMI088_ACCL_RANGE_24G			0x03
+#define BMI088_ACCL_PWR_CONF		 	0x7C
+#define BMI088_ACCL_PWR_CTRL		  	0x7D
 
 #define BMI088_GYRO_CHIP_ID		        0x00
+#define BMI088_GYRO_DATA		  		0x02
+#define BMI088_GYRO_CHIP_ID_VALUE		0x0F
+#define BMI088_GYRO_RANGE				0x0F
+#define BMI088_GYRO_RANGE_2000			0x00
 
 /* bmi088 reg value set */
 #define BMI088_ACC_PWR_CTRL_ACC_ENABLE  0x04
 #define BMI088_ACC_PWR_CTRL_ACC_DISABLE	0x00
 
+#define BMI088_ACCEL_3G_SEN 0.0008974358974f
+#define BMI088_ACCEL_6G_SEN 0.00179443359375f
+#define BMI088_ACCEL_12G_SEN 0.0035888671875f
+#define BMI088_ACCEL_24G_SEN 0.007177734375f
+
+
+#define BMI088_GYRO_2000_SEN 0.00106526443603169529841533860381f
+#define BMI088_GYRO_1000_SEN 0.00053263221801584764920766930190693f
+#define BMI088_GYRO_500_SEN 0.00026631610900792382460383465095346f
+#define BMI088_GYRO_250_SEN 0.00013315805450396191230191732547673f
+#define BMI088_GYRO_125_SEN 0.000066579027251980956150958662738366f
+
+extern bmi088_t bmi088;
+
 int bmi088_read(bmi088_sensor_type_t sensor_type, uint8_t reg, uint8_t *reg_data);
 int bmi088_write(bmi088_sensor_type_t sensor_type, uint8_t reg, const uint8_t reg_data);
+
+int bmi088_init(void);
+int bmi088_update_data(void);
 
 #endif /* !BMI088_H */
--- Core/Src/bmi088.c
+++ Core/Src/bmi088.c
@@ -3,9 +3,13 @@
 /* include spi read & write file */
 #include "spi.h"
 
+#include <math.h>
+
+bmi088_t bmi088;
+
 int bmi088_read(bmi088_sensor_type_t sensor_type, uint8_t reg, uint8_t *reg_data)
 {
-	spi_cs_t cs = (sensor_type == BMI088_ACCE) ? SPI3_CS1 : SPI3_CS2;
+	spi_cs_t cs = (sensor_type == BMI088_ACCL) ? SPI3_CS1 : SPI3_CS2;
 
 	uint8_t tx_data[3] = {0};
 	uint8_t rx_data[3] = {0};
@@ -13,19 +17,102 @@ int bmi088_read(bmi088_sensor_type_t sensor_type, uint8_t reg, uint8_t *reg_data
 	tx_data[0] = reg | 0x80;
 	spi_write_read(cs, &tx_data[0], &rx_data[0], 3);
 
-	*reg_data = (sensor_type == BMI088_ACCE) ? rx_data[2] : rx_data[1];
+	*reg_data = (sensor_type == BMI088_ACCL) ? rx_data[2] : rx_data[1];
 	return 0;
 }
 
 int bmi088_write(bmi088_sensor_type_t sensor_type, uint8_t reg, const uint8_t reg_data)
 {
-	spi_cs_t cs = (sensor_type == BMI088_ACCE) ? SPI3_CS1 : SPI3_CS2;
+	spi_cs_t cs = (sensor_type == BMI088_ACCL) ? SPI3_CS1 : SPI3_CS2;
 
 	uint8_t data[2] = {0};
 	/*  first btye's bit 0 set 0, it's mean write */
 	data[0] = reg & 0x7F;
 	data[1] = reg_data;
 	spi_write(cs, &data[0], 2);
+
+	return 0;
+}
+
+int bmi088_read_bytes(bmi088_sensor_type_t sensor_type, uint8_t reg, uint8_t *data, uint8_t data_length)
+{
+	int i = 0;
+	while (data_length--)
+	{
+		bmi088_read(sensor_type, reg++, &data[i++]);
+	}
+
+	return 0;
+}
+
+int bmi088_init(void)
+{
+	int result = 0;
+	uint8_t bmi088_accl_range = 0;
+	uint8_t bmi088_gyro_range = 0;
+	uint8_t bmi088_accl_chip_id = 0;
+	uint8_t bmi088_gyro_chip_id = 0;
+
+	bmi088_write(BMI088_ACCL, BMI088_ACCL_PWR_CTRL, BMI088_ACC_PWR_CTRL_ACC_ENABLE);
+	HAL_Delay(1);
+
+	bmi088_write(BMI088_ACCL, BMI088_ACCL_RANGE, BMI088_ACCL_RANGE_24G);
+	HAL_Delay(1);
+	bmi088_read(BMI088_ACCL, BMI088_ACCL_RANGE, &bmi088_accl_range);
+	HAL_Delay(1);
+	if (bmi088_accl_range != BMI088_ACCL_RANGE_24G) {
+		result += 1;
+	}
+	bmi088_write(BMI088_GYRO, BMI088_GYRO_RANGE, BMI088_GYRO_RANGE_2000);
+	HAL_Delay(1);
+	bmi088_read(BMI088_GYRO, BMI088_GYRO_RANGE, &bmi088_gyro_range);
+	if (bmi088_gyro_range != BMI088_GYRO_RANGE_2000) {
+		result += 1;
+	}
+	HAL_Delay(1);
+
+	bmi088_read(BMI088_ACCL, BMI088_ACCL_CHIP_ID, &bmi088_accl_chip_id);
+	HAL_Delay(1);
+	bmi088_read(BMI088_GYRO, BMI088_GYRO_CHIP_ID, &bmi088_gyro_chip_id);
+	HAL_Delay(1);
+
+	if (!(bmi088_accl_chip_id == BMI088_ACCL_CHIP_ID_VALUE &&
+		  bmi088_gyro_chip_id == BMI088_GYRO_CHIP_ID_VALUE))
+	{
+		result += 1;
+	}
+
+	return result;
+}
+
+int bmi088_update_data(void)
+{
+	uint8_t accl_data[6] = {0};
+	uint8_t gyro_data[6] = {0};
+
+	bmi088_read_bytes(BMI088_ACCL, BMI088_ACCL_DATA, &accl_data[0], 6);
+	bmi088_read_bytes(BMI088_GYRO, BMI088_GYRO_DATA, &gyro_data[0], 6);
+
+	bmi088.origin_accl_data[0] = (int16_t)accl_data[1] << 8 | accl_data[0];
+	bmi088.origin_accl_data[1] = (int16_t)accl_data[3] << 8 | accl_data[2];
+	bmi088.origin_accl_data[2] = (int16_t)accl_data[5] << 8 | accl_data[4];
+	bmi088.origin_gyro_data[0] = (int16_t)gyro_data[1] << 8 | gyro_data[0];
+	bmi088.origin_gyro_data[1] = (int16_t)gyro_data[3] << 8 | gyro_data[2];
+	bmi088.origin_gyro_data[2] = (int16_t)gyro_data[5] << 8 | gyro_data[4];
+
+	// bmi088.imu.accl[0]         = (float)bmi088.origin_accl_data[0] / 32768 * 1000 * pow(2, BMI088_ACCL_RANGE_24G + 1) * 1.5f;
+	// bmi088.imu.accl[1]         = (float)bmi088.origin_accl_data[1] / 32768 * 1000 * pow(2, BMI088_ACCL_RANGE_24G + 1) * 1.5f;
+	// bmi088.imu.accl[2]         = (float)bmi088.origin_accl_data[2] / 32768 * 1000 * pow(2, BMI088_ACCL_RANGE_24G + 1) * 1.5f;
+	bmi088.imu.accl[0]         = bmi088.origin_accl_data[0] * BMI088_ACCEL_24G_SEN;
+	bmi088.imu.accl[1]         = bmi088.origin_accl_data[1] * BMI088_ACCEL_24G_SEN;
+	bmi088.imu.accl[2]         = bmi088.origin_accl_data[2] * BMI088_ACCEL_24G_SEN;
+
+	// bmi088.imu.gyro[0]         = (float)bmi088.origin_gyro_data[0] / 32768 * 2000.0f;
+	// bmi088.imu.gyro[1]         = (float)bmi088.origin_gyro_data[1] / 32768 * 2000.0f;
+	// bmi088.imu.gyro[2]         = (float)bmi088.origin_gyro_data[2] / 32768 * 2000.0f;
+	bmi088.imu.gyro[0]         = (float)bmi088.origin_gyro_data[0] * BMI088_GYRO_2000_SEN;
+	bmi088.imu.gyro[1]         = (float)bmi088.origin_gyro_data[1] * BMI088_GYRO_2000_SEN;
+	bmi088.imu.gyro[2]         = (float)bmi088.origin_gyro_data[2] * BMI088_GYRO_2000_SEN;
 
 	return 0;
 }
