356fac027efcda302d53af96b77ac76bc6a66422
lucky-sushishu <sjcsushishu@outlook.com>
2026-06-26 00:30:30 +0800

    控制：加入姿态控制和位置控制模块

---
+++ application/ctrl/srs_attitude_control/srs_attitude_control.c
@@ -0,0 +1,11 @@
+/******************************************************************************/
+/* Copyright (c) 2026 Star Ring Studio. All rights reserved.                  */
+/*                                                                            */
+/* This file is part of the Star Ring Studio (SRS) project and is licensed    */
+/* under the terms specified in the LICENSE file included with this project.  */
+/*                                                                            */
+/* Unauthorized copying of this file, via any medium, is strictly prohibited. */
+/* Proprietary and confidential.                                              */
+/******************************************************************************/
+
+#include "srs_attitude_control.h"
---
+++ application/ctrl/srs_position_control/srs_position_control.h
@@ -0,0 +1,14 @@
+/******************************************************************************/
+/* Copyright (c) 2026 Star Ring Studio. All rights reserved.                  */
+/*                                                                            */
+/* This file is part of the Star Ring Studio (SRS) project and is licensed    */
+/* under the terms specified in the LICENSE file included with this project.  */
+/*                                                                            */
+/* Unauthorized copying of this file, via any medium, is strictly prohibited. */
+/* Proprietary and confidential.                                              */
+/******************************************************************************/
+
+#ifndef SRS_ATTITUDE_CONTROL_H
+#define SRS_ATTITUDE_CONTROL_H
+
+#endif /* SRS_ATTITUDE_CONTROL_H */
